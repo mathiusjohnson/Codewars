166fd20befce33aa9550db2537383d2ff09ef58e
Mathius Johnson <devachievem@gmail.com>
2020-12-03 20:49:18 -0800

    non repeating letters passed

--- nonrepeatletters.js
+++ nonrepeatletters.js
@@ -1,15 +1,60 @@
 function firstNonRepeatingLetter(s) {
-  // Add your code here
-//   console.log(s)
-  if (s === '') {
-    return s
-  }
-
-  for (let i = 0; i < s.length; i++) {
-    let c = s.charAt(i);
-    if (s.indexOf(c) == i && s.indexOf(c, i + 1) == -1) {
-      return c;
+  const results = {};
+  let sArr = s.split('')
+  for (let i = 0; i < sArr.length; i++) {
+    const to_lower = sArr[i].toLowerCase()
+    if (to_lower !== " ") {
+      if (results[to_lower] == undefined)
+        results[to_lower] = [];
+      results[to_lower].push(i);
     }
   }
-  return 'null';
+  let char = ''
+  for (let key in results) {
+    if (results[key].length === 1) {
+      char = key
+      break
+    }
+  }
+  for (let i = 0; i < sArr.length; i++) {
+    if (sArr[i].toLowerCase() == char) {
+      console.log(sArr[i])
+      char = sArr[i]
+    }
+	}
+	  
+  return char;
+
+}
+
+function firstNonRepeatingLetter(s) {
+	for(var i in s) {
+		if(s.match(new RegExp(s[i],"gi")).length === 1) {
+			return s[i];
+		}
+	}
+	return '';
+}
+
+function firstNonRepeatingLetter(s) {
+	var t=s.toLowerCase();
+	for (var x=0;x<t.length;x++)
+		if(t.indexOf(t[x]) === t.lastIndexOf(t[x]))
+			return s[x];
+	return "";
+}
+
+
+function firstNonRepeatingLetter(str){
+  return str.split('').find(e => str.match(new RegExp(`${e}`, 'gi')).length === 1) || ''
+}
+
+function firstNonRepeatingLetter(s) {
+  let str = s.toLowerCase();
+  for(let i = 0; i < str.length; i++) {
+    if(str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+      return s[i];
+    }
+  }
+  return "";
 }
